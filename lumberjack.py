45543101b3463ec234db472994036ff413cf3dd6
sirCharlesJohnson <johcha7@yahoo.com>
2015-03-24 10:06:33 -0400

    Delete lumberjack.py

--- lumberjack.py
+++
@@ -1,6 +0,0 @@
-name = input("What your name? ")
-
-if name == "Charles":
- print("{} is a lumberjack and he/she's OK!".format(name))
-else:
-  print("{} sleeps all night and {} works all day!".format(name, name))

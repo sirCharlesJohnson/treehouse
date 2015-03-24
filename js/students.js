3a5b50ca758ce34042ae93f2317c7caceb9bdce5
Charles Johnson <johcha7@yahoo.com>
2015-03-24 09:26:09 -0400

    Createa Dungeon Game.

---
+++ js/students.js
@@ -0,0 +1,45 @@
+var students = [
+  {
+    name: 'Charles Johnson',
+    track: 'Java, JavaScript',
+    achievements: 2,
+    points: 1745
+  },
+  
+   { 
+    name: 'Edna Johnson',
+    track: 'Web Design',
+    achievements: 1,
+    points: 709
+  },
+  
+   { 
+    name: 'Michael Swarze',
+    track: 'Web Design, JavaScript, HTML, CSS',
+    achievements: 4,
+    points: 2785
+  },
+  
+   { 
+    name: 'Tom Jones',
+    track: 'Java, JavaScript, Ruby, WordPress',
+    achievements: 4,
+    points: 2723
+  },
+  
+   { 
+    name: 'Mark Smith',
+    track: 'Java, JavaScript, PHP',
+    achievements: 3,
+    points: 2100
+  },
+  
+  {
+    name: 'Charles Johnson',
+    track: 'Ruby, PHP, WordPress',
+    achievements: 5,
+    points: 3000
+  }
+  
+];
+
---
+++ lumberjack.py
@@ -0,0 +1,6 @@
+name = input("What your name? ")
+
+if name == "Charles":
+ print("{} is a lumberjack and he/she's OK!".format(name))
+else:
+  print("{} sleeps all night and {} works all day!".format(name, name))
